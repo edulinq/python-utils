cb95745cf9faaff130e76e9797c6fa87499a69a2
BatuhanSA <basalihcs@gmail.com>
2025-10-09 10:00:16 -0700

    Revised the set testing structure and edited some help messages.

--- edq/cli/config/set.py
+++ edq/cli/config/set.py
@@ -3,6 +3,7 @@ Set a configuration option.
 """
 
 import argparse
+import os
 import sys
 import typing
 
@@ -11,10 +12,11 @@ import edq.core.config
 import edq.util.dirent
 import edq.util.json
 
-def write_configs_to_file(file_path: str, configs_to_write: typing.List[str]) -> None:
+def write_config_to_file(file_path: str, configs_to_write: typing.List[str]) -> None:
     """ Write configs to a specified file path. Create the path if it do not exist. """
 
     if (not (edq.util.dirent.exists(file_path))):
+        edq.util.dirent.mkdir(os.path.dirname(file_path))
         edq.util.json.dump_path({}, file_path)
 
     config = edq.util.json.load_path(file_path)
@@ -38,19 +40,19 @@ def write_configs_to_file(file_path: str, configs_to_write: typing.List[str]) ->
 def run_cli(args: argparse.Namespace) -> int:
     """ Run the CLI. """
 
-    if (not (args.write_to_local or args.write_to_global or (args.file_to_write is not None))):
-        args.write_to_local = True
+    if (not (args.set_is_local or args.set_is_global or (args.set_to_file_path is not None))):
+        args.set_is_local = True
 
-    if (args.write_to_local):
+    if (args.set_is_local):
         local_config_path = args._config_params.get(edq.core.config.LOCAL_CONFIG_PATH_KEY)
         if (local_config_path is None):
             local_config_path = args._config_params.get(edq.core.config.FILENAME_KEY)
-        write_configs_to_file(local_config_path, args.config_to_set)
-    elif (args.write_to_global):
+        write_config_to_file(local_config_path, args.config_to_set)
+    elif (args.set_is_global):
         global_config_path = args._config_params.get(edq.core.config.GLOBAL_CONFIG_PATH_KEY)
-        write_configs_to_file(global_config_path, args.config_to_set)
-    elif (args.file_to_write is not None):
-        write_configs_to_file(args.file_to_write, args.config_to_set)
+        write_config_to_file(global_config_path, args.config_to_set)
+    elif (args.set_to_file_path is not None):
+        write_config_to_file(args.set_to_file_path, args.config_to_set)
 
     return 0
 
@@ -72,24 +74,28 @@ def modify_parser(parser: argparse.ArgumentParser) -> None:
 
     parser.add_argument('config_to_set', metavar = "<KEY>=<VALUE>",
         action = 'store', nargs = '+', type = str,
-        help = 'Configuration option to be set. Expected config format is <key>=<value>.',
+        help = ('Configuration option to be set. Expected config format is <key>=<value>.'),
     )
 
     group = parser.add_mutually_exclusive_group()
 
     group.add_argument('--local',
-        action = 'store_true', dest = 'write_to_local',
-        help = 'Sets the variable in local config file.',
+        action = 'store_true', dest = 'set_is_local',
+        help = ('Set the configuration option in a local config file if one exists. '
+        + ' If no local config file is found, a new one will be created in the current directory.'),
     )
 
     group.add_argument('--global',
-        action = 'store_true', dest = 'write_to_global',
-        help = 'Sets the variable in global config file.',
+        action = 'store_true', dest = 'set_is_global',
+        help =  ('Set the configuration option in the global config file if it exists.'
+        +  " If it doesn't exist, it will be created."
+        +  " Use '--config-global' to view or change the global config file location."),
     )
 
-    group.add_argument('--file',
-        action = 'store', type = str, default = None, dest = 'file_to_write',
-        help = 'Sets the variable in a specified config file.',
+    group.add_argument('--file', metavar = "<FILE>",
+        action = 'store', type = str, default = None, dest = 'set_to_file_path',
+        help = ('Set the config option in a specified config file.'
+            +  " If the given file path doesn't exist, it will be created.")
     )
 
 if (__name__ == '__main__'):
--- edq/cli/config/set_test.py
+++ edq/cli/config/set_test.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import typing
 
 import edq.testing.unittest
 import edq.cli.config.set
@@ -45,79 +46,221 @@ def create_test_dir(temp_dir_prefix: str) -> str:
 
     return temp_dir
 
+def replace_placeholders_str(unresolved_object: str, placeholder: str, replacement: str) -> str:
+    """
+    In-place replacement of a placeholder in a string with a given replacement.
+    If the input is not a string, no changes are made and the original object is returned.
+    """
+
+    if(isinstance(unresolved_object, str)):
+        unresolved_object = unresolved_object.replace(placeholder, replacement)
+
+    return unresolved_object
+
+def replace_placeholders_dict(unresolved_object: dict, placeholder: str, replacement: str) -> dict:
+    """ Performs an in-place, recursive replacement of placeholder with replacement across all levels of a nested dictionary. """
+
+    if(not isinstance(unresolved_object, dict)):
+        raise TypeError(f"Expected unresolved object to be dict, got {type(unresolved_object).__name__}")
+
+    for element in unresolved_object:
+        if (isinstance(unresolved_object[element], dict)):
+            unresolved_object[element] = replace_placeholders_dict(unresolved_object[element], placeholder = placeholder, replacement = replacement)
+        if (isinstance(unresolved_object[element], str)):
+            unresolved_object[element] = replace_placeholders_str(unresolved_object[element], placeholder = placeholder, replacement = replacement)
+
+    return unresolved_object
+
+def replace_tempdir_placeholder(
+        cli_args: typing.Dict[str, typing.Union[str, typing.List, bool]],
+        expected_result: typing.List,
+        temp_dir_path: str,
+    ) -> typing.Tuple[typing.Dict[str, typing.Union[str, typing.List]], typing.List[typing.Dict[str, typing.Union[str, typing.Dict[str, str]]]]]:
+    """ Replaces all occurrences of 'TEMP_DIR' in CLI arguments and expected results with the actual temporary directory path. """
+
+
+    resolved_expected_result = []
+    for file in expected_result:
+        set_to_file_path = file.get("path")
+        data = file.get("data")
+        set_to_file_path = replace_placeholders_str(set_to_file_path, 'TEMP_DIR', temp_dir_path)
+        resolved_expected_result.append({
+            "path": set_to_file_path,
+            "data": data
+        })
+
+    resolved_cli_args = replace_placeholders_dict(cli_args, 'TEMP_DIR', temp_dir_path)
+    return resolved_cli_args, resolved_expected_result
+
 class TestSetConfig(edq.testing.unittest.BaseTest):
     """ Test basic functionality of set config. """
 
     def test_set_base(self):
         """
-        Test that the set command creates configuration files and writes the specified configuration correctly.
+        Test that the 'set' command creates configuration files and writes the specified configuration correctly.
+        'TEMP_DIR' gets replaced with actual temp dir path when testing.
         """
 
         # [(set cli arguments, expected result, error substring), ...]
         test_cases = [
+            # Invalid Option
+
+            # Empty Key
+            (
+                {
+                    "config_to_set": ["=user@test.edulinq.org"],
+                },
+                [],
+                "Found an empty configuration option key associated with the value 'user@test.edulinq.org'.",
+            ),
+
+            # Missing '='
+            (
+                {
+                    "config_to_set": ["useruser@test.edulinq.org"],
+                },
+                [],
+                "Configuration options must be provided in the format `<key>=<value>` when passed via the CLI.",
+            ),
+
+            # Local Config
+
+            # No Config
             (
                 {
                     "config_to_set": ["user=user@test.edulinq.org"],
                 },
-                (
+                [
                     {
-                        "path": os.path.join("TEMP_DIR", edq.core.config.DEFAULT_CONFIG_FILENAME),
+                        "path": os.path.join('TEMP_DIR', edq.core.config.DEFAULT_CONFIG_FILENAME),
                         "data": {"user": "user@test.edulinq.org"},
                     },
-                ),
+                ],
                 None,
-            )
+            ),
 
+            # Empty Config
+            (
+                {
+                    "config_to_set": ["user=user@test.edulinq.org"],
+                    "_config_params": {
+                        edq.core.config.LOCAL_CONFIG_PATH_KEY: os.path.join("TEMP_DIR", "empty-config", edq.core.config.DEFAULT_CONFIG_FILENAME),
+                    },
+                },
+                [
+                    {
+                        "path": os.path.join("TEMP_DIR", "empty-config", edq.core.config.DEFAULT_CONFIG_FILENAME),
+                        "data": {"user": "user@test.edulinq.org"},
+                    },
+                ],
+                None,
+            ),
+
+            # Non Empty Config
+            (
+                {
+                    "config_to_set": ["pass=password123"],
+                    "_config_params": {
+                        edq.core.config.LOCAL_CONFIG_PATH_KEY: os.path.join("TEMP_DIR", "non-empty-config", edq.core.config.DEFAULT_CONFIG_FILENAME),
+                    },
+                },
+                [
+                    {
+                        "path": os.path.join("TEMP_DIR", "non-empty-config", edq.core.config.DEFAULT_CONFIG_FILENAME),
+                        "data": {"user": "user@test.edulinq.org", "pass": "password123"},
+                    },
+                ],
+                None,
+            ),
+
+            # Global Config
+
+            # No Config
+            (
+                {
+                    "config_to_set": ["user=user@test.edulinq.org"],
+                    "set_is_global": True,
+                },
+                [
+                    {
+                        "path": os.path.join("TEMP_DIR", GLOBAL_DIR, edq.core.config.DEFAULT_CONFIG_FILENAME),
+                        "data": {"user": "user@test.edulinq.org"},
+                    },
+                ],
+                None,
+            ),
+
+            # File Config
+
+            # Non Existent File
+            (
+                {
+                    "config_to_set": ["user=user@test.edulinq.org"],
+                    "set_to_file_path": os.path.join("TEMP_DIR", "non-existent", "path", edq.core.config.DEFAULT_CONFIG_FILENAME),
+                },
+                [
+                    {
+                        "path": os.path.join("TEMP_DIR", "non-existent", "path", edq.core.config.DEFAULT_CONFIG_FILENAME),
+                        "data": {"user": "user@test.edulinq.org"},
+                    },
+                ],
+                None,
+            ),
         ]
 
-        for test_case in test_cases:
-            temp_dir = create_test_dir(temp_dir_prefix = "edq-test-config-set-")
-
+        for (i, test_case) in enumerate(test_cases):
             cli_arguments, expected_result, error_substring = test_case
 
-            set_args = argparse.Namespace(
-                write_to_local = cli_arguments.get("write_to_local", False),
-                write_to_global = cli_arguments.get("write_to_global", False),
-                file_to_write = cli_arguments.get("file_to_write", None),
-                config_to_set = cli_arguments.get("config_to_set"),
-                _config_params = {
-                    edq.core.config.LOCAL_CONFIG_PATH_KEY: cli_arguments.get(edq.core.config.LOCAL_CONFIG_PATH_KEY, None),
-                    edq.core.config.FILENAME_KEY: cli_arguments.get(edq.core.config.FILENAME_KEY, edq.core.config.DEFAULT_CONFIG_FILENAME),
-                    edq.core.config.GLOBAL_CONFIG_PATH_KEY: cli_arguments.get(
-                        edq.core.config.GLOBAL_CONFIG_PATH_KEY,
-                        os.path.join(temp_dir, GLOBAL_DIR)
-                    )
-                }
-            )
+            with self.subTest(msg = f"Case {i}"):
+                temp_dir = create_test_dir(temp_dir_prefix = "edq-test-config-set-")
 
-            error_substring = None
+                (cli_arguments, expected_result) =  replace_tempdir_placeholder(cli_arguments, expected_result, temp_dir)
 
-            previous_work_directory = os.getcwd()
-            os.chdir(temp_dir)
+                config_params = cli_arguments.get("_config_params", {})
+                filename = config_params.get(edq.core.config.FILENAME_KEY, edq.core.config.DEFAULT_CONFIG_FILENAME)
 
-            try:
-                edq.cli.config.set.run_cli(set_args)
-            except Exception as ex:
-                error_string = self.format_error_string(ex)
+                set_args = argparse.Namespace(
+                    set_is_local = cli_arguments.get("set_is_local", False),
+                    set_is_global = cli_arguments.get("set_is_global", False),
+                    set_to_file_path = cli_arguments.get("set_to_file_path", None),
+                    config_to_set = cli_arguments.get("config_to_set"),
+                    _config_params = {
+                        edq.core.config.LOCAL_CONFIG_PATH_KEY: config_params.get(edq.core.config.LOCAL_CONFIG_PATH_KEY, None),
+                        edq.core.config.GLOBAL_CONFIG_PATH_KEY: config_params.get(
+                            edq.core.config.GLOBAL_CONFIG_PATH_KEY,
+                            os.path.join(temp_dir, GLOBAL_DIR, filename),
+                        ),
+                        edq.core.config.FILENAME_KEY: filename
+                    }
+                )
 
-                if (error_substring is None):
-                    self.fail(f"Unexpected error: '{error_string}'.")
+                previous_work_directory = os.getcwd()
+                os.chdir(temp_dir)
 
-                self.assertIn(error_substring, error_string, 'Error is not as expected.')
+                try:
+                    edq.cli.config.set.run_cli(set_args)
+                except Exception as ex:
+                    error_string = self.format_error_string(ex)
 
-            finally:
-                os.chdir(previous_work_directory)
+                    if (error_substring is None):
+                        self.fail(f"Unexpected error: '{error_string}'.")
 
-            if (error_substring is not None):
-                self.fail(f"Did not get expected error: '{error_substring}'.")
+                    self.assertIn(error_substring, error_string, 'Error is not as expected.')
 
-            for file in expected_result:
-                file_path = file.get("path")
-                file_path = file_path.replace('TEMP_DIR', temp_dir)
+                    continue
 
-                if (not edq.util.dirent.exists(file_path)):
-                    self.fail("Expected file doesn't exist.")
+                finally:
+                    os.chdir(previous_work_directory)
 
-                data_actual = edq.util.json.load_path(file_path)
+                if (error_substring is not None):
+                    self.fail(f"Did not get expected error: '{error_substring}'.")
 
-                self.assertJSONDictEqual(data_actual, file.get("data"))
+                for file in expected_result:
+                    set_to_file_path = file.get("path")
+
+                    if (not edq.util.dirent.exists(set_to_file_path)):
+                        self.fail(f"Expected file does not exist at path: {set_to_file_path}")
+
+                    data_actual = edq.util.json.load_path(set_to_file_path)
+
+                    self.assertJSONDictEqual(data_actual, file.get("data"))
