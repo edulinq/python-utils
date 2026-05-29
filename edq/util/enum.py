d28b346b2912dfa87abb2a30fe2957847df97706
Eriq Augustine <eriq.augustine@gmail.com>
2026-05-29 09:31:01 -0500

    Added an additional check for enum contents.

--- edq/util/enum.py
+++ edq/util/enum.py
@@ -4,5 +4,8 @@ import typing
 def has_value(enum_class: typing.Type[enum.Enum], value: typing.Any) -> bool:
     """ Check if the given value is one of the possible values for the given enum. """
 
+    if (value in enum_class):
+        return True
+
     enum_values = {member.value for member in enum_class}
     return (value in enum_values)
